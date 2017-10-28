9256ac9938e99eb4918629a9ffc658b67b70a3d3
Guilherme Hiroshi Yoshii Caris <carishiroshi@gmail.com>
2017-10-28 11:05:33 -0300

    Update Main.java

--- Combinacao/src/atividade/Main.java
+++ Combinacao/src/atividade/Main.java
@@ -7,85 +7,62 @@ public class Main {
     public static int con = 0;
 
     public static void main(String[] args) {
-        int cont =1;
-        n = Integer.parseInt((JOptionPane.showInputDialog("Digite quantas posição sera ultilizado")));
-        int numero[] = new int[10];
-        int vetor[] = new int[n];
+        CombinacaoCom2Numero combinacaoCom2Numero = new CombinacaoCom2Numero();
+        CombinacaoCom3Numero combinacaoCom3Numero = new CombinacaoCom3Numero();
+        CombinacaoCom4Numero combinacaoCom4Numero = new CombinacaoCom4Numero();
+        CombinacaoCom5Numero combinacaoCom5Numero = new CombinacaoCom5Numero();
+        CombinacaoCom6numero combinacaoCom6numero = new CombinacaoCom6numero();
+        CombinacaoCom7Numero combinacaoCom7Numero = new CombinacaoCom7Numero();
+        CombinacaoCom8Numero combinacaoCom8Numero = new CombinacaoCom8Numero();
+        CombinacaoCOm9Numero combinacaoCOm9Numero = new CombinacaoCOm9Numero();
+        CombinacaoCom10Numero combinacaoCom10Numero = new CombinacaoCom10Numero();
 
+        n = Integer.parseInt((JOptionPane.showInputDialog("Digite")));
+
+        int numero[] = new int[n];
+        int vetor[] = new int[10];
 
         for (int i = 0; i < numero.length; i++) {
-            numero[i] = Integer.parseInt((JOptionPane.showInputDialog("digie o numero" +cont++)));
+            numero[i] = Integer.parseInt((JOptionPane.showInputDialog("digie o numero")));
         }
+
         if (n == 2) {
-            combinacaodedoisnumeros(numero, vetor);
+            combinacaoCom2Numero.combo2(numero, vetor);
+
         }
         if (n == 3) {
-            combinacaodetresnumeros(numero, vetor);
+            combinacaoCom3Numero.combo3(numero, vetor);
         }
         if (n == 4) {
-            combinacaocomquatro(numero, vetor);
+            combinacaoCom4Numero.combo4(numero, vetor);
         }
         if (n == 5) {
-            combinacaocomcinco(numero, vetor);
+            combinacaoCom5Numero.combo5(numero, vetor);
         }
         if (n == 6) {
-            combinacaocomseis(numero, vetor);
+            combinacaoCom6numero.combo6(numero, vetor);
         }
         if (n == 7) {
-            combinacaocomsete(numero, vetor);
+            combinacaoCom7Numero.combo7(numero, vetor);
+
         }
         if (n == 8) {
-            combinacaocomoito(numero, vetor);
+            combinacaoCom8Numero.combo8(numero, vetor);
         }
         if (n == 9) {
-            combinacaocomnove(numero, vetor);
+            combinacaoCOm9Numero.combo9(numero, vetor);
         }
         if (n == 10) {
-            combinacaocomdez(numero, vetor);
+            combinacaoCom10Numero.combo10(numero, vetor);
         }
-        if (n < 0 ){
+        if (n < 0) {
             System.out.println("ERRO : o numero de posição ultilizado e menor que 0");
         }
-        if (n > 10){
+        if (n > 10) {
             System.out.println("ERRO : o numero de posição ultilizado e mairo que 10, podera travar o seu Computador");
         }
     }
 
-
-    public static void combinacaodedoisnumeros(int[] numero, int vetor[]) {
-
-        for (int i = 0; i < numero.length; i++) {
-            vetor[0] = numero[i];
-            for (int j = 0; j < numero.length; j++) {
-                vetor[1] = numero[j];
-
-                con++;
-                System.out.println("Combinação " + con + ": " + vetor[0] + "-" + vetor[1]);
-
-            }
-        }
-
-
-    }
-
-    public static void combinacaodetresnumeros(int[] numeros, int vetor[]) {
-
-        for (int a = 0; a < numeros.length; a++) {
-            vetor[0] = numeros[a];
-            for (int b = 0; b < numeros.length; b++) {
-                vetor[1] = numeros[b];
-                for (int c = 0; c < numeros.length; c++) {
-                    vetor[2] = numeros[c];
-                    con++;
-
-                    System.out.println("Combinação " + con + ": " + vetor[0]
-                            + "-" + vetor[1]
-                            + "-" + vetor[2]);
-
-                }
-            }
-        }
-    }
 
     public static void combinacaocomquatro(int numero[], int vetor[]) {
         for (int i = 0; i < numero.length; i++) {
@@ -106,8 +83,70 @@ public class Main {
             }
         }
     }
+}
 
-    public static void combinacaocomcinco(int numero[], int vetor[]) {
+class CombinacaoCom2Numero {
+    public void combo2(int numero[], int vetor[]) {
+
+        int con = 0;
+        for (int i = 0; i < numero.length; i++) {
+            vetor[0] = numero[i];
+            for (int j = 0; j < numero.length; j++) {
+                vetor[1] = numero[j];
+                con++;
+                System.out.println("Combinação " + con + ": " + vetor[0] + "-" + vetor[1]);
+
+            }
+        }
+
+    }
+}
+
+class CombinacaoCom3Numero {
+    int con = 0;
+
+    public void combo3(int numero[], int vetor[]) {
+        for (int i = 0; i < numero.length; i++) {
+            vetor[0] = numero[i];
+            for (int j = 0; j < numero.length; j++) {
+                vetor[1] = numero[j];
+                for (int k = 0; k < numero.length; k++) {
+                    vetor[2] = numero[k];
+                    con++;
+                    System.out.println("Combinação " + con + ": " + vetor[0] + "-" + vetor[1] + "-" + vetor[2]);
+                }
+            }
+        }
+    }
+}
+
+class CombinacaoCom4Numero {
+
+    public void combo4(int numero[], int vetor[]) {
+        int con = 0;
+        for (int i = 0; i < numero.length; i++) {
+            vetor[0] = numero[i];
+            for (int j = 0; j < numero.length; j++) {
+                vetor[1] = numero[j];
+                for (int k = 0; k < numero.length; k++) {
+                    vetor[2] = numero[k];
+                    for (int l = 0; l < numero.length; l++) {
+                        vetor[3] = numero[l];
+                        con++;
+                        System.out.println("Combinação " + con + ": " + vetor[0]
+                                + "-" + vetor[1]
+                                + "-" + vetor[2]
+                                + "-" + vetor[3]);
+                    }
+                }
+            }
+        }
+    }
+}
+
+class CombinacaoCom5Numero {
+    public void combo5(int numero[], int vetor[]) {
+        int con = 0;
         for (int i = 0; i < numero.length; i++) {
             vetor[0] = numero[i];
             for (int j = 0; j < numero.length; i++) {
@@ -130,9 +169,13 @@ public class Main {
                 }
             }
         }
-    }
 
-    public static void combinacaocomseis(int numero[], int vetor[]) {
+    }
+}
+
+class CombinacaoCom6numero {
+    public void combo6(int numero[], int vetor[]) {
+        int con = 0;
         for (int i = 0; i < numero.length; i++) {
             vetor[0] = numero[i];
             for (int j = 0; j < numero.length; i++) {
@@ -160,9 +203,13 @@ public class Main {
             }
 
         }
-    }
 
-    public static void combinacaocomsete(int numero[], int vetor[]) {
+    }
+}
+
+class CombinacaoCom7Numero {
+    public void combo7(int numero[], int vetor[]) {
+        int con = 0;
         for (int i = 0; i < numero.length; i++) {
             vetor[0] = numero[i];
             for (int j = 0; j < numero.length; i++) {
@@ -195,8 +242,11 @@ public class Main {
         }
 
     }
+}
 
-    public static void combinacaocomoito(int numero[], int vetor[]) {
+class CombinacaoCom8Numero {
+    public void combo8(int numero[], int vetor[]) {
+        int con = 0;
         for (int i = 0; i < numero.length; i++) {
             vetor[0] = numero[i];
             for (int j = 0; j < numero.length; i++) {
@@ -231,8 +281,11 @@ public class Main {
             }
         }
     }
+}
 
-    public static void combinacaocomnove(int numero[], int vetor[]) {
+class CombinacaoCOm9Numero {
+    public void combo9(int numero[], int vetor[]) {
+        int con = 0;
         for (int i = 0; i < numero.length; i++) {
             vetor[0] = numero[i];
             for (int j = 0; j < numero.length; i++) {
@@ -274,7 +327,11 @@ public class Main {
         }
     }
 
-    public static void combinacaocomdez(int numero[], int vetor[]) {
+}
+
+class CombinacaoCom10Numero {
+    public void combo10(int numero[], int vetor[]) {
+        int con = 0;
         for (int i = 0; i < numero.length; i++) {
             vetor[0] = numero[i];
             for (int j = 0; j < numero.length; i++) {
@@ -318,7 +375,6 @@ public class Main {
                 }
             }
         }
+
     }
 }
-
-
